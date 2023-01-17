a4a803479fece0c7e3cfb1ebe8fac6ce2053f952
petra <petra@pop-os.localdomain>
2023-01-17 16:04:20 +0100

    Refactor: rename the functions.

--- cli/commands.py
+++ cli/commands.py
@@ -5,7 +5,7 @@ python -m cli.commands show_multiple --image_path "data/test/" --num_img 7
 
 import click
 from cli import options
-from utils.show_image import plot_multiple, plot_one, plot_many
+from utils import visualize
 
 
 @click.group(invoke_without_command=True)
@@ -18,21 +18,21 @@ def cli():
 @cli.command("show_one")
 @options.image_path
 def show_one(image_path: str):
-    plot_one(image_path=image_path)
+    visualize.show_image(image_path=image_path)
 
 
 @cli.command("show_multiple")
 @options.image_path
 @options.num_images
 def show_multiple(image_path: str, num_img: int):
-    plot_multiple(folder_path=image_path, num_img=num_img)
+    visualize.show_multiple(folder_path=image_path, num_img=num_img)
 
 
 @cli.command("show_many")
 @options.image_path
 @options.num_images
 def show_many(image_path: str, num_img: int):
-    plot_many(folder_path=image_path, num_img=num_img)
+    visualize.show_many(folder_path=image_path, num_img=num_img)
 
 
 if __name__ == "__main__":
