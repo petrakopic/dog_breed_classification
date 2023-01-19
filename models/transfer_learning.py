87b904dfc948dfeb7f8f6050e5e2f9dc93aeab67
petra <petra@pop-os.localdomain>
2023-01-19 16:34:40 +0100

    Refactor: refactor models.

--- models/transfer_learning.py
+++ models/transfer_learning.py
@@ -9,37 +9,48 @@ def combine_models(
     training: bool = True,
 ):
     """
-    Combine the results of the pretrained resnet50 model.
-    (https://www.tensorflow.org/api_docs/python/tf/keras/applications/resnet50/ResNet50)
-    and the custom model. The last layers of both networks are global average pooling
-    followed by softmax with the number of classes given by num_classes.
+    Combine the results of the pretrained resnet50 model
+    (https://www.tensorflow.org/api_docs/python/tf/keras/applications/resnet50/ResNet50),
+    xception model (https://www.tensorflow.org/api_docs/python/tf/keras/applications/xception/Xception)
+    and the custom model.
     :return:
     """
 
     input_img = tf.keras.Input(shape=input_shape)
     input_img = data_augmenter()(input_img)
 
-    base_model = model_from_resnet(input_shape=input_shape, num_classes=num_classes)
-    output1 = base_model(input_img)
+    resnet_model = model_from_resnet(input_shape=input_shape)
+    output_resnet = resnet_model(input_img)
+
+    xception_model = model_from_xception(input_shape=input_shape)
+    output_xception = xception_model(input_img)
 
     custom_model = conv_skip_model_block(
-        input_shape=input_shape, num_classes=num_classes, training=training
+        input_shape=input_shape, training=training
     )
-    output2 = custom_model(input_img)
+    output_custom = custom_model(input_img)
+    output_combined = tf.keras.layers.concatenate([output_resnet, output_xception, output_custom], axis=-1)
 
-    output = tf.keras.layers.Add()([output1, output2])
+    x = tf.keras.layers.GlobalAveragePooling2D()(output_combined)
+
+    # Add dropout to avoid overfitting
+    if training:
+        x = tf.keras.layers.Dropout(0.4)(x)
+
+    output = tf.keras.layers.Dense(units=num_classes, activation="softmax")(x)
     model = tf.keras.Model(input_img, output)
+
     return model
 
 
 def conv_skip_model_block(
     input_shape: Tuple[int, int, int],
-    num_classes: int = 120,
     training: bool = True,
 ):
     """
-    Model consists of three blocks Conv->BatchNorm->Relu and one skip connection.
-    Number of trainable parameters is 804
+    Model consists of three blocks Conv->BatchNorm->Relu
+    Number of trainable parameters is: 3338
+    The output shape of is (7,7,20)
     :param input_shape:
     :param num_classes: Number of classes in the target variable
     :param training: True is the model is in the training phase, otherwise False.
@@ -47,36 +58,23 @@ def conv_skip_model_block(
     """
 
     input_img = tf.keras.Input(shape=input_shape)
-    input_shortcut = input_img
 
     x = _conv_block(
         input_img,
         filters=3,
         kernel_size=1,
-        strides=(1, 1),
+        strides=(2, 2),
         padding="valid",
         training=training,
     )
 
     x = _conv_block(
-        x, filters=3, kernel_size=4, strides=(1, 1), padding="same", training=training
+        x, filters=10, kernel_size=1, strides=(4, 4), padding="valid", training=training
     )
 
     x = _conv_block(
-        x, filters=3, kernel_size=4, strides=(1, 1), padding="same", training=training
+        x, filters=20, kernel_size=4, strides=(4, 4), padding="valid", training=training
     )
-
-    x = tf.keras.layers.Add()([x, input_shortcut])
-
-    # Decrease the dimension before flattening:
-    x = tf.keras.layers.AveragePooling2D(
-        pool_size=4, strides=2, padding="valid", data_format="channels_last"
-    )(x)
-
-    # Global average pooling flattens the input to the num_channels (use instead of FCL to avoid
-    # over fitting)
-    x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(units=num_classes, activation="softmax")(x)
 
     model = tf.keras.Model(inputs=input_img, outputs=x)
     return model
@@ -102,7 +100,7 @@ def _conv_block(
     return x
 
 
-def model_from_resnet(input_shape: Optional[Tuple] = None, num_classes: int = 120):
+def model_from_resnet(input_shape: Optional[Tuple] = None):
     """
     load pretrained ResNet50 model. The model is trained to classify different objects.
     :param num_classes: Number of classes in the target variable
@@ -116,15 +114,35 @@ def model_from_resnet(input_shape: Optional[Tuple] = None, num_classes: int = 12
         input_tensor=None,
         input_shape=input_shape,
         pooling=None,
-        classes=500,
+        classes=1000,
     )
 
     base_model.trainable = False
     x = base_model(input_img)
 
-    x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    output = tf.keras.layers.Dense(units=num_classes, activation="softmax")(x)
-
-    model = tf.keras.Model(inputs=input_img, outputs=output)
+    model = tf.keras.Model(inputs=input_img, outputs=x)
     return model
+
+
+def model_from_xception(input_shape: Optional[Tuple] = None):
+    """
+    load pretrained ResNet50 model. The model is trained to classify different objects.
+    :param num_classes: Number of classes in the target variable
+    :param input_shape:
+    :return:
+    """
+    input_img = tf.keras.Input(shape=input_shape)
+    base_model = tf.keras.applications.xception.Xception(
+        include_top=False,
+        weights="imagenet",
+        input_tensor=None,
+        input_shape=input_shape,
+        pooling=None,
+        classes=500,
+    )
+
+    base_model.trainable = False
+    x = base_model(input_img)
+    model = tf.keras.Model(inputs=input_img, outputs=x)
+    return model
+
