afd1db882fdc37cb079c22a5a0a7197a80f8603d
petra <petra@pop-os.localdomain>
2023-01-24 18:28:54 +0100

    Feat: add predicting function

--- models/development/pretrained_features.py
+++ models/development/pretrained_features.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 import tensorflow as tf
 from tqdm import tqdm
 
@@ -19,7 +19,6 @@ PREPROCESS_FUNCTIONS = (xcep_preprocess, resnet_preprocess, inception_preprocess
 
 def main(
     folder_path: str,
-    base_learning_rate,
     model_out_path: str,
     batch_size: int = 10,
     epochs: int = 10,
@@ -27,15 +26,32 @@ def main(
     target = prepare_target.run(folder_path=folder_path)
     features = prepare_features(folder_path=folder_path)
 
-    model = custom_model(input_shape=features.shape[1:], training=True)
+    model = custom_model(input_shape=features.shape[1])
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
-        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+        optimizer=tf.keras.optimizers.Adam(),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
         metrics=["accuracy"],
     )
     model.fit(x=features, y=target, batch_size=batch_size, epochs=epochs)
     model.save(model_out_path)
     return model
+
+
+def predict(folder_path: str, model_out_path: str) -> List[str]:
+    """
+
+    :param folder_path: folder to the test set
+    :param model_out_path: path to the pretrained model
+    :return:
+    """
+    model = tf.keras.models.load_model(model_out_path)
+
+    input_images = prepare_features(folder_path=folder_path)
+
+    predicted = model.predict(input_images)
+    predicted = [(i == i.max()).astype(int) for i in predicted]
+    class_names = [prepare_target.target_to_class_name(p) for p in predicted]
+    return class_names
 
 
 def prepare_features(
@@ -69,14 +85,9 @@ def prepare_features(
 
 def custom_model(
     input_shape: Tuple[int, int, int],
-    training: bool = True,
 ):
     input_img = tf.keras.Input(shape=input_shape)
-
-    if training:
-        x = tf.keras.layers.Dropout(0.7)(input_img)
-    else:
-        x = input_img
-    x = tf.keras.layers.Dense(units=120, activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.7)(input_img)
+    x = tf.keras.layers.Dense(units=120, activation="softmax")(x)
     model = tf.keras.Model(inputs=input_img, outputs=x)
     return model
