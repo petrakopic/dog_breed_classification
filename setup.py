6e15e680b6901866d3f531e40bd707a4f5995ee4
petra <petra@pop-os.localdomain>
2023-01-24 15:51:54 +0100

    Choire: add dependency package.

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     name="dog_breed_identification",
     description="Module for training and validating ML models on the dataset from the kaggle competition",
     long_description=_open_readme("README.md"),
-    install_requires=["tensorflow", "numpy", "click", "matplotlib"],
+    install_requires=["tensorflow", "numpy", "click", "matplotlib", "tqdm"],
     python_requires=">=3.7",
 )
